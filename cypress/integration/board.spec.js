e36d8b084d72de1b2923768a06d42049531f6302
Arbab Khan <arbab.tufail@ocloudsolutions.net>
2023-08-02 15:40:26 +0500

    Upadated the Trello board E2E test

--- cypress/integration/board.spec.js
+++ cypress/integration/board.spec.js
@@ -7,12 +7,14 @@ describe("board dashboard", () => {
     cy.visit("/");
   });
 
-  it("should add a new coloumn and create new cards and add them into the columns and a drag card from To Do to In Progress then to Done to new column", () => {
+  it("should add a new coloumn and create new cards and add them into the columns, update the card content, and a drag card from To Do to In Progress then to Done to new column", () => {
+    // Adding new Analysis Column
     cy.contains("+ Add another list").click();
     cy.get("[placeholder='List title']").type("Analysis");
     cy.contains("Add list").click();
     cy.get('[data-cy="column-component"]').should("have.length", 4);
 
+    // Adding four Cards in To Do Column
     cy.get("[data-cy='column-component']")
       .eq(0)
       .within(() => {
@@ -29,33 +31,43 @@ describe("board dashboard", () => {
         cy.contains("Add Card").click();
 
         cy.contains("+ Add Card").click();
-        cy.get("[placeholder='Add Card description']").type("Task4");
+        cy.get("[placeholder='Add Card description']").type(
+          "This card will be deleted"
+        );
         cy.contains("Add Card").click();
       });
 
+    // updating First Card Content
     cy.get("[data-cy='column-component']")
       .eq(0)
-      .contains("Task1")
-      .should("exist");
+      .within(() => {
+        cy.get("[data-cy='card-component']")
+          .eq(0)
+          .within(() => {
+            cy.contains("Edit").click();
+            cy.get("textarea").type(" Updated");
+            cy.contains("Save").click();
+          });
+      });
 
     cy.get("[data-cy='column-component']").eq(1).as("inProgressColumn");
 
     const dataTransfer = new DataTransfer();
 
+    // Moved Task1 Card to In progress Column
     cy.get("[data-cy='column-component']")
       .eq(0)
       .get("[data-cy='card-component']")
       .contains("Task1")
       .trigger("dragstart", { dataTransfer });
-
     cy.get("@inProgressColumn").trigger("drop", { dataTransfer });
 
+    // Moved Task2 Card to In progress Column
     cy.get("[data-cy='column-component']")
       .eq(0)
       .get("[data-cy='card-component']")
       .contains("Task2")
       .trigger("dragstart", { dataTransfer });
-
     cy.get("@inProgressColumn").trigger("drop", { dataTransfer });
 
     cy.get("[data-cy='column-component']")
@@ -63,8 +75,8 @@ describe("board dashboard", () => {
       .contains("Task1")
       .should("exist");
 
+    // Moved Task1 Card from In Progress Column to Done Column
     cy.get("[data-cy='column-component']").eq(2).as("DoneColumn");
-
     cy.get("[data-cy='column-component']")
       .eq(1)
       .within(() => {
@@ -72,7 +84,6 @@ describe("board dashboard", () => {
           .contains("Task1")
           .trigger("dragstart", { dataTransfer });
       });
-
     cy.get("@DoneColumn").trigger("drop", { dataTransfer });
 
     cy.get("[data-cy='column-component']")
@@ -80,8 +91,8 @@ describe("board dashboard", () => {
       .contains("Task1")
       .should("exist");
 
+    // Moved Task1 Card from Done Column to Analysis Column
     cy.get("[data-cy='column-component']").eq(3).as("AnalysisColumn");
-
     cy.get("[data-cy='column-component']")
       .eq(2)
       .within(() => {
@@ -96,5 +107,16 @@ describe("board dashboard", () => {
       .eq(3)
       .contains("Task1")
       .should("exist");
+
+    //Deleted the last Card in To Do Column
+    cy.get("[data-cy='column-component']")
+      .eq(0)
+      .within(() => {
+        cy.get("[data-cy='card-component']")
+          .last()
+          .within(() => {
+            cy.contains("Delete").click();
+          });
+      });
   });
 });
