9de3406617f1c7e6f541d77608287a7ba0452924
Arbab Khan <arbab.tufail@ocloudsolutions.net>
2023-08-07 20:34:58 +0500

    Mocked the Redux

--- src/component/board/__test__/board.test.tsx
+++ src/component/board/__test__/board.test.tsx
@@ -1,107 +1,60 @@
 import React from "react";
-import configureMockStore from "redux-mock-store";
-import { screen, render, fireEvent, waitFor } from "@testing-library/react";
+import { screen, fireEvent, waitFor } from "@testing-library/react";
+
 import Board from "../board";
-import { IColumn } from "../../../store/column/column.types";
-import { Provider } from "react-redux";
-import columnsReducer from "../../../store/column/column.reducer";
-import { addColumn } from "../../../store/column/column.action";
-import { ADD_COLUMN } from "../../../store/column/column.types";
+import { renderWithProviders } from "../../utils/util";
 
 describe("testing the board component", () => {
-  let mockInitialColumns: IColumn[];
-  let Store: any;
-  beforeEach(() => {
-    mockInitialColumns = [
-      {
-        id: "todo",
-        title: "To Do",
-        cards: [],
-      },
-      {
-        id: "inProgress",
-        title: "In Progress",
-        cards: [],
-      },
-      {
-        id: "done",
-        title: "Done",
-        cards: [],
-      },
-    ];
-    const mockStore = configureMockStore([]); // You can add middleware if needed
-    Store = mockStore({
-      columns: mockInitialColumns,
-    });
-    // const mockStore = configureMockStore(); // You can add middleware if needed
-    // Store = mockStore(mockInitialColumns);
+  test("Board should render initial columns and adding list option correctly", () => {
+    renderWithProviders(<Board />);
+    const columnTitles = ["To Do", "In Progress", "Done"];
+
+    for (const title of columnTitles) {
+      const columnElement = screen.getByText(title);
+      expect(columnElement).toBeInTheDocument();
+    }
+
+    const addColumnButton = screen.getByText("+ Add another list");
+    expect(addColumnButton).toBeInTheDocument();
   });
-  // test("Board should render initial columns and adding list option correctly", () => {
-  //   render(
-  //     <Provider store={Store}>
-  //       <Board />
-  //     </Provider>
-  //   );
-  //   const columnTitles = ["To Do", "In Progress", "Done"];
+  test("Clicking the 'Add another list' button should show the text are and buttons for adding a new column", () => {
+    renderWithProviders(<Board />);
+    const addButton = screen.getByText("+ Add another list");
+    fireEvent.click(addButton);
 
-  //   for (const title of columnTitles) {
-  //     const columnElement = screen.getByText(title);
-  //     expect(columnElement).toBeInTheDocument();
-  //   }
+    const textarea = screen.getByPlaceholderText("List title");
+    const addListButton = screen.getByRole("button", { name: "Add list" });
+    const removeButton = screen.getByRole("button", { name: "Remove" });
+    expect(textarea).toBeInTheDocument();
+    expect(addListButton).toBeInTheDocument();
+    expect(removeButton).toBeInTheDocument();
+  });
 
-  //   const addColumnButton = screen.getByText("+ Add another list");
-  //   expect(addColumnButton).toBeInTheDocument();
-  // });
-  // test("Clicking the 'Add another list' button should show the text are and buttons for adding a new column", () => {
-  //   render(
-  //     <Provider store={Store}>
-  //       <Board />
-  //     </Provider>
-  //   );
-  //   const addButton = screen.getByText("+ Add another list");
-  //   fireEvent.click(addButton);
+  test("Entering an empty title in the textarea and clicking the 'Add list' button should not add any new column to the board", () => {
+    renderWithProviders(<Board />);
+    const addButton = screen.getByText("+ Add another list");
+    fireEvent.click(addButton);
 
-  //   const textarea = screen.getByPlaceholderText("List title");
-  //   const addListButton = screen.getByRole("button", { name: "Add list" });
-  //   const removeButton = screen.getByRole("button", { name: "Remove" });
-  //   expect(textarea).toBeInTheDocument();
-  //   expect(addListButton).toBeInTheDocument();
-  //   expect(removeButton).toBeInTheDocument();
-  // });
+    const textarea = screen.getByPlaceholderText("List title");
+    const addListButton = screen.getByRole("button", { name: "Add list" });
+    const removeButton = screen.getByRole("button", { name: "Remove" });
+    expect(textarea).toBeInTheDocument();
+    expect(addListButton).toBeInTheDocument();
+    expect(removeButton).toBeInTheDocument();
 
-  // test("Entering an empty title in the textarea and clicking the 'Add list' button should not add any new column to the board", () => {
-  //   render(
-  //     <Provider store={Store}>
-  //       <Board />
-  //     </Provider>
-  //   );
-  //   const addButton = screen.getByText("+ Add another list");
-  //   fireEvent.click(addButton);
+    expect(textarea).toHaveValue("");
+    fireEvent.click(addListButton);
 
-  //   const textarea = screen.getByPlaceholderText("List title");
-  //   const addListButton = screen.getByRole("button", { name: "Add list" });
-  //   const removeButton = screen.getByRole("button", { name: "Remove" });
-  //   expect(textarea).toBeInTheDocument();
-  //   expect(addListButton).toBeInTheDocument();
-  //   expect(removeButton).toBeInTheDocument();
+    expect(textarea).toBeInTheDocument();
+    expect(addListButton).toBeInTheDocument();
+    expect(removeButton).toBeInTheDocument();
 
-  //   expect(textarea).toHaveValue("");
-  //   fireEvent.click(addListButton);
-
-  //   expect(textarea).toBeInTheDocument();
-  //   expect(addListButton).toBeInTheDocument();
-  //   expect(removeButton).toBeInTheDocument();
-
-  //   const newColumn = screen.queryByText("New Column");
-  //   expect(newColumn).not.toBeInTheDocument();
-  // });
+    const newColumn = screen.queryByText("New Column");
+    expect(newColumn).not.toBeInTheDocument();
+  });
 
   test("Entering a valid title in textare and clicking the 'Add list' button should add a new column to the board with the provided title", async () => {
-    render(
-      <Provider store={Store}>
-        <Board />
-      </Provider>
-    );
+    renderWithProviders(<Board />);
     const addButton = screen.getByText("+ Add another list");
     fireEvent.click(addButton);
 
@@ -114,65 +67,42 @@ describe("testing the board component", () => {
     console.log("input after change:", inputField.value);
     fireEvent.click(addListButton);
 
-    // await waitFor(() => {
+    await waitFor(() => {
+      const newColumn = screen.getByText("New Column");
+      expect(newColumn).toBeInTheDocument();
+    });
     const newColumn = screen.getByText("New Column");
     expect(newColumn).toBeInTheDocument();
-    // });
-
-    const NewColumn = {
-      id: expect.any(String),
-      title: "New Column",
-      cards: [],
-    };
-    const expectedAction = {
-      type: ADD_COLUMN,
-      payload: newColumn,
-    };
-    Store.dispatch(expectedAction); 
-    expect(Store.getActions()).toContainEqual(expectedAction);
   });
-  // const newColumn = screen.queryByText("New Column");
-  // console.log("querybyTExt ::::", newColumn);
-  // expect(newColumn).not.toBeInTheDocument();
-  // test("Clicking the 'Remove' button should hide the input field and the 'Add list' and 'Remove' buttons", () => {
-  //   render(
-  //     <Provider store={Store}>
-  //       <Board />
-  //     </Provider>
-  //   );
-  //   const addButton = screen.getByText("+ Add another list");
-  //   fireEvent.click(addButton);
+  test("Clicking the 'Remove' button should hide the input field and the 'Add list' and 'Remove' buttons", () => {
+    renderWithProviders(<Board />);
+    const addButton = screen.getByText("+ Add another list");
+    fireEvent.click(addButton);
 
-  //   const inputField = screen.getByPlaceholderText("List title");
-  //   const addListButton = screen.getByText("Add list");
-  //   const removeButton = screen.getByText("Remove");
+    const inputField = screen.getByPlaceholderText("List title");
+    const addListButton = screen.getByText("Add list");
+    const removeButton = screen.getByText("Remove");
 
-  //   fireEvent.change(inputField, { target: { value: "New Column" } });
-  //   fireEvent.click(removeButton);
+    fireEvent.change(inputField, { target: { value: "New Column" } });
+    fireEvent.click(removeButton);
 
-  //   expect(inputField).not.toBeInTheDocument();
-  //   expect(addListButton).not.toBeInTheDocument();
-  //   expect(removeButton).not.toBeInTheDocument();
-  // });
-  // test("Add new column to the board", async () => {
-  //   render(
-  //     <Provider store={Store}>
-  //       <Board />
-  //     </Provider>
-  //   );
-  //   const addColumnButton = screen.getByText("+ Add another list");
-  //   fireEvent.click(addColumnButton);
+    expect(inputField).not.toBeInTheDocument();
+    expect(addListButton).not.toBeInTheDocument();
+    expect(removeButton).not.toBeInTheDocument();
+  });
+  test("Add new column to the board", async () => {
+    renderWithProviders(<Board />);
+    const addColumnButton = screen.getByText("+ Add another list");
+    fireEvent.click(addColumnButton);
 
-  //   const textareaElement = screen.getByPlaceholderText("List title");
-  //   const addButton = screen.getByRole("button", { name: "Add list" });
-  //   screen.debug();
-  //   fireEvent.change(textareaElement, {
-  //     target: { value: "helo" },
-  //   });
-  //   fireEvent.click(addButton);
-  //   screen.debug();
+    const textareaElement = screen.getByPlaceholderText("List title");
+    const addButton = screen.getByRole("button", { name: "Add list" });
+    fireEvent.change(textareaElement, {
+      target: { value: "helo" },
+    });
+    fireEvent.click(addButton);
 
-  //   const newColumn = await screen.findByText("helo");
-  //   expect(newColumn).toBeInTheDocument();
-  // });
+    const newColumn = await screen.findByText("helo");
+    expect(newColumn).toBeInTheDocument();
+  });
 });
